13be988140350dc21cc51f31d35cfea0596dab4a
Yago Militão <yagolano@hotmail.com>
2026-09-05 18:06:40 -0300

    feat: tornar as propriedades children das layouts como Readonly

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from 'react'
 
-export default function PublicLayout({ children }: { children: ReactNode }) {
+export default function PublicLayout({ children }: Readonly<{ children: ReactNode }>) {
   return <>{children}</>
 }
